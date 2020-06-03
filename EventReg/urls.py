afa65e8aea0373762aed6b2db9ab8770d02e64c3
adarshjhaa100 <31096082+adarshjhaa100@users.noreply.github.com>
2020-06-03 20:53:21 +0530

    Updated Events

--- EventReg/urls.py
+++ EventReg/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',include('reg.urls')),
-    path('eventReg/',include('adminEventRegistration.urls'))
+    path('adminLogin/',include('adminEventRegistration.urls')),
+    path('',views.index,name='home')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
