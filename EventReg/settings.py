04dd176ec1ace3e89a664827f8c24ef6eeb2f5e0
Aditya Choudhary <adityachoudhary54@gmail.com>
2020-08-21 21:45:13 +0530

    Created er diagram for the model

--- EventReg/settings.py
+++ EventReg/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'reg.apps.RegConfig',
     'adminEventRegistration'
 ]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
